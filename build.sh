ed100a27c4a4bfa3c55c1031b411031d75c0d11a
rsundahl <roy@polyverse.io>
2018-01-26 07:46:18 -0800

    Delete all files for an empty tag.

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker build -t readhook .
-
-# Extract the library from the container
-docker run -d --rm --name readhook readhook sleep 10
-docker cp readhook:/readhook/readhook.so $PWD/readhook.so
-docker cp readhook:/readhook/obj $PWD
-docker kill readhook
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm --name readhook readhook /readhook/readhook

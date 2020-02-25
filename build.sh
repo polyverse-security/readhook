30a1e8d4980d859ba19e3b6eaf108bab5e2ab365
rsundahl <roy@polyverse.com>
2020-02-25 10:40:42 -0800

    Use latest build/test/publish patterns.

--- build.sh
+++ build.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+
 # Delete the old artifacts
-rm -r app dll
+rm -r app dll lib obj
+
+# Make the output directories
+mkdir -p app dll lib obj
+
+# Remove the build container (if it was left around)
+docker rm -f readhook
 
 # Build readhook
 docker build --no-cache -t readhook .
 
 # Run readhook and just sleep while we copy the build artifacts
-docker run -d --rm --name readhook readhook sleep 60
+docker run --name readhook readhook
 
-# Extract the buld artifacts
-docker cp readhook:/readhook/dll/ $PWD/dll/
+# Extract the build artifacts
 docker cp readhook:/readhook/app/ $PWD/app/
+docker cp readhook:/readhook/dll/ $PWD/dll/
+docker cp readhook:/readhook/lib/ $PWD/lib/
+docker cp readhook:/readhook/obj/ $PWD/obj/
 
-# We're done so kill it since it's just sleeping
-docker kill readhook
+# Remove the build container
+docker rm -f readhook
